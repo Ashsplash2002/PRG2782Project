40aaaabed218a9fa223910029ebc924ba05c377d
Ashsplash2002 <86288374+Ashsplash2002@users.noreply.github.com>
2022-11-10 15:45:19 +0200

    changes to delete form

--- Data Access Layer/DataHandler.cs
+++ Data Access Layer/DataHandler.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 
 namespace PRG2782Project
@@ -13,173 +14,273 @@ namespace PRG2782Project
     internal class DataHandler
     {
 
-        //might need to change settings in Sql Configuration Manager. 'SQL Server' must be running. Enable TCP/IP.
-        string connect = @"Data Source=(local)\SQLEXPRESS; Initial Catalog=;Integrated Security=sspi";
-        SqlConnection conn;
-        SqlCommand cmd;
-        SqlDataReader rdr;
+    //    //might need to change settings in Sql Configuration Manager. 'SQL Server' must be running. Enable TCP/IP.
+    //    string connect = @"Data Source=(local)\SQLEXPRESS; Initial Catalog=;Integrated Security=sspi";
+    //    SqlConnection conn;
+    //    SqlCommand cmd;
+    //    SqlDataReader rdr;
 
 
 
 
-        List<Module> modules = new List<Module>();
-        List<Student> students = new List<Student>();
-        List<Login> logins = new List<Login>();
+    //    List<Module> modules = new List<Module>();
+    //    List<Student> students = new List<Student>();
+    //    List<Login> logins = new List<Login>();
 
 
-        //shouldn't these go in file handler class? There are 6 here.
-        public void WriteStudent(string studentNumber, string studentName, string studentSurname, string studentImage, string studentGender, string studentPhone, string studentAddress, string moduleCodes, DateTime studentDOB)
-        {
-            string queryinsert = @"Insert into students  Values('" + studentNumber + "','" + studentName + "','" + studentSurname + "','" + studentImage + "','" + studentGender +
-                "','" + studentPhone + "','" + studentAddress + "','" + moduleCodes + "','" + studentDOB + "')";
-            conn = new SqlConnection(connect);
-            conn.Open();
-            cmd = new SqlCommand(queryinsert, conn);
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Student details saved successfully");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Details not saved: " + ex);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+    //    //shouldn't these go in file handler class? There are 6 here.
+    //    public void WriteStudent(string studentNumber, string studentName, string studentSurname, string studentImage, string studentGender, string studentPhone, string studentAddress, string moduleCodes, DateTime studentDOB)
+    //    {
+    //        string queryinsert = @"Insert into students  Values('" + studentNumber + "','" + studentName + "','" + studentSurname + "','" + studentImage + "','" + studentGender +
+    //            "','" + studentPhone + "','" + studentAddress + "','" + moduleCodes + "','" + studentDOB + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryinsert, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Student details saved successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not saved: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
 
-        public List<Module> ReadModule()
-        {
-            return modules;
-        }
+    //    public List<Module> ReadModule()
+    //    {
+    //        string viewquery = @"Select * from tblStudents";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(searchquery, conn);
+
+    //        List<Student> list = new List<Student>();
+    //        Student obj = new Student();
+    //        try
+    //        {
+    //            rdr = cmd.ExecuteReader();
+
+    //            while (rdr.Read())
+    //            {
+    //                obj.StudentID = rdr[0].ToString();
+    //                obj.Name = rdr[1].ToString();
+    //                obj.Surname = rdr[2].ToString();
+    //                obj.CourseID = rdr[3].ToString();
+
+    //                list.Add(new Student(obj.StudentID, obj.Name, obj.Surname, obj.CourseID));
+    //            }
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Something went wrong: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //        return list;
+    //    }
 
 
 
-        public void WriteModule(string moduleCode, string moduleName, string moduleDescription, string moduleLink)
-        {
-            string queryinsert = @"Insert into modules Values('" + moduleCode + "','" + moduleName + "','" + moduleDescription + "','" + moduleLink + "')";
-            conn = new SqlConnection(connect);
-            conn.Open();
-            cmd = new SqlCommand(queryinsert, conn);
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Module details saved successfully");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Details not saved: " + ex);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+    //    public void WriteModule(string moduleCode, string moduleName, string moduleDescription, string moduleLink)
+    //    {
+    //        string queryinsert = @"Insert into modules Values('" + moduleCode + "','" + moduleName + "','" + moduleDescription + "','" + moduleLink + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryinsert, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Module details saved successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not saved: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
 
-        public List<Student> ReadStudent()
-        {
-            return students;
-        }
+    //    public List<Student> ReadStudent()
+    //    {
+    //        string viewquery = @"Select * from tblStudents";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(searchquery, conn);
+
+    //        List<Student> list = new List<Student>();
+    //        Student obj = new Student();
+    //        try
+    //        {
+    //            rdr = cmd.ExecuteReader();
+
+    //            while (rdr.Read())
+    //            {
+    //                obj.StudentID = rdr[0].ToString();
+    //                obj.Name = rdr[1].ToString();
+    //                obj.Surname = rdr[2].ToString();
+    //                obj.CourseID = rdr[3].ToString();
+
+    //                list.Add(new Student(obj.StudentID, obj.Name, obj.Surname, obj.CourseID));
+    //            }
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Something went wrong: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //        return list;
+    //    }
 
 
 
 
 
-        //4 DataHandler Methods Added:
-        public void studentDelete(string studentID)
-        {
-            string queryinsert = @"Delete from students Where StudentID=('" + studentID + "')";
-            conn = new SqlConnection(connect);
-            conn.Open();
-            cmd = new SqlCommand(queryinsert, conn);
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Student details deleted successfully");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Details not saved: " + ex);
-            }
-            finally
-            {
-                conn.Close();
-            }
+    //    //4 DataHandler Methods Added:
+    //    public void studentDelete(string studentID)
+    //    {
+    //        string queryDelete = @"Delete from students Where StudentID=('" + studentID + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryinsert, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Student details deleted successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not saved: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
 
-        }
+    //    }
 
-        public void studentSearch(string studentID)
-        {
-            SqlDataReader rdr = null;
+    //    public void studentSearch(string studentID)
+    //    {
+    //        SqlDataReader rdr = null;
 
-            string querySearch = @"Select * from students Where id=('" + studentID + "')";
+    //        string querySearch = @"Select * from students Where id=('" + studentID + "')";
 
-            conn.Open();
+    //        conn.Open();
 
-            SqlCommand cmd = new SqlCommand(querySearch, conn);
+    //        SqlCommand cmd = new SqlCommand(querySearch, conn);
 
-            try
-            {
-                rdr = cmd.ExecuteReader();
-                MessageBox.Show("Search successful");
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Search failed");
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+    //        try
+    //        {
+    //            rdr = cmd.ExecuteReader();
+    //            MessageBox.Show("Search successful");
+    //        }
+    //        catch (Exception)
+    //        {
+    //            MessageBox.Show("Search failed");
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
 
-        public void moduleDelete(string moduleID)
-        {
-            string queryinsert = @"Delete from modules Where StudentID=('" + moduleID + "')";
-            conn = new SqlConnection(connect);
-            conn.Open();
-            cmd = new SqlCommand(queryinsert, conn);
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Student details deleted successfully");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Details not saved: " + ex);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+    //    public void moduleDelete(string moduleID)
+    //    {
+    //        string queryDelete = @"Delete from modules Where StudentID=('" + moduleID + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryinsert, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Student details deleted successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not saved: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
 
-        public void studentUpdate(string studentID, string studentName, string studentSurname, string studentDOB, string studentGender, string studentPhone, string studentAddress, string moduleCode)
-        {
-            string queryUpdate = @"Update students Set stdName = @studentName, stdSurname = @studentSurname " + "Where id = @studentID";
-            conn = new SqlConnection(connect);
-            conn.Open();
-            cmd = new SqlCommand(queryUpdate, conn);
+    //    public void studentUpdate(string studentID, string studentName, string studentSurname, string studentDOB, string studentGender, string studentPhone, string studentAddress, string moduleCode)
+    //    {
+    //        string queryUpdate = @"Update tblStudents Set StudentID=('" + studentID + "'), StudentName=('" + name + "'), LastName=('" + surname + "'), CourseID=('" + courseID + "') Where StudentID=('" + studentID + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryUpdate, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Student details update successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not updated: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
+    //    public void moduleUpdate()
+    //    {
+    //        string queryUpdate = @"Update tblStudents Set StudentID=('" + studentID + "'), StudentName=('" + name + "'), LastName=('" + surname + "'), CourseID=('" + courseID + "') Where StudentID=('" + studentID + "')";
+    //        conn = new SqlConnection(connect);
+    //        conn.Open();
+    //        cmd = new SqlCommand(queryUpdate, conn);
+    //        try
+    //        {
+    //            cmd.ExecuteNonQuery();
+    //            MessageBox.Show("Student details update successfully");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            MessageBox.Show("Details not updated: " + ex);
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
 
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Student name updated successfully");
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Student name not updated");
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
-        public void moduleUpdate()
-        {
+    //    public void moduleSearch(string studentID)
+    //    {
+    //        SqlDataReader rdr = null;
 
-        }
+    //        string querySearch = @"Select * from students Where id=('" + studentID + "')";
+
+    //        conn.Open();
+
+    //        SqlCommand cmd = new SqlCommand(querySearch, conn);
+
+    //        try
+    //        {
+    //            rdr = cmd.ExecuteReader();
+    //            MessageBox.Show("Search successful");
+    //        }
+    //        catch (Exception)
+    //        {
+    //            MessageBox.Show("Search failed");
+    //        }
+    //        finally
+    //        {
+    //            conn.Close();
+    //        }
+    //    }
     }
 }
--- Presentation Layer/frmDeleteModule.Designer.cs
+++ Presentation Layer/frmDeleteModule.Designer.cs
@@ -34,13 +34,16 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvdelete = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdelete)).BeginInit();
             this.SuspendLayout();
             // 
             // btnback
             // 
             this.btnback.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnback.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnback.Location = new System.Drawing.Point(313, 154);
+            this.btnback.Location = new System.Drawing.Point(436, 312);
             this.btnback.Name = "btnback";
             this.btnback.Size = new System.Drawing.Size(149, 33);
             this.btnback.TabIndex = 28;
@@ -52,7 +55,7 @@
             // 
             this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDelete.Location = new System.Drawing.Point(28, 154);
+            this.btnDelete.Location = new System.Drawing.Point(28, 300);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(94, 33);
             this.btnDelete.TabIndex = 27;
@@ -70,7 +73,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(21, 25);
+            this.label6.Location = new System.Drawing.Point(101, 28);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(422, 39);
             this.label6.TabIndex = 25;
@@ -95,12 +98,35 @@
             this.label2.TabIndex = 22;
             this.label2.Text = "Enter Module Code: ";
             // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Location = new System.Drawing.Point(491, 89);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 33);
+            this.btnSearch.TabIndex = 29;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // dgvdelete
+            // 
+            this.dgvdelete.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvdelete.Location = new System.Drawing.Point(28, 135);
+            this.dgvdelete.Name = "dgvdelete";
+            this.dgvdelete.RowHeadersWidth = 51;
+            this.dgvdelete.RowTemplate.Height = 24;
+            this.dgvdelete.Size = new System.Drawing.Size(557, 150);
+            this.dgvdelete.TabIndex = 30;
+            // 
             // frmDeleteModule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(505, 252);
+            this.ClientSize = new System.Drawing.Size(620, 365);
+            this.Controls.Add(this.dgvdelete);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnback);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.txtModuleCode);
@@ -110,6 +136,8 @@
             this.Name = "frmDeleteModule";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmDeleteModule";
+            this.Load += new System.EventHandler(this.frmDeleteModule_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdelete)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,5 +151,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvdelete;
     }
 }
