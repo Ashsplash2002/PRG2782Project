cba585349863e8e5c0a7fd42763f414654c7ef71
Ashsplash2002 <86288374+Ashsplash2002@users.noreply.github.com>
2022-11-10 09:17:13 +0200

    added new forms for update delete and search

---
+++ Presentation Layer/frmDeleteModule.cs
@@ -0,0 +1,28 @@
+﻿using PRG2782Project.Presentation_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG2782Project
+{
+    public partial class frmDeleteModule : Form
+    {
+        public frmDeleteModule()
+        {
+            InitializeComponent();
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            frmModuleNEW module = new frmModuleNEW();
+            module.Show();
+            this.Close();
+        }
+    }
+}
